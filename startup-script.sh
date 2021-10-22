022bf73fd4bde1f73dbe7f5ca1b0917aeae34738
jrchan4 <justinrichardchan@gmail.com>
2021-10-22 01:23:15 -0700

    startup-script for GCE

--- startup-script.sh
+++ startup-script.sh
@@ -1,17 +1,14 @@
-
 set -v
 
 # [START startup]
 # Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
-REPOSITORY=""
+REPOSITORY="github_ubcdsc_ttnr"
 
 # Install logging monitor. The monitor will automatically pick up logs sent to
 # syslog.
-# [START logging]
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
-# [END logging]
 
 # Install dependencies from apt
 apt-get install -yq ca-certificates git nodejs build-essential supervisor
@@ -33,6 +30,8 @@ cd /opt/app/${REPOSITORY}
 npm install
 cat >./.env << EOF
 DISCORD_TOKEN=ODk5NDAwNzAzMTEyNTE5Njkw.YWyOLQ.AkXlfcBHBCmUiDhYLTu6gWodLlw
+CLIENT_ID=899400703112519690
+GUILD_ID=901004061996904450
 EOF
 
 # Create a nodeapp user. The application will run as this user.
