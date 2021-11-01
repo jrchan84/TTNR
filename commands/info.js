4c634f9884f23b662c05c37508fa31f1eebe869d
yungalyx <52767711+yungalyx@users.noreply.github.com>
2021-11-01 15:50:35 -0700

    can reply with embed object

--- commands/info.js
+++ commands/info.js
@@ -1,12 +1,26 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, channelMention } = require('@discordjs/builders');
+const { MessageEmbed, Channel } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('info')
 		.setDescription('learn more about this bot!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
-    console.log('TTNR replied pong to: ' + interaction.user.tag
-			+ ' in server: ' + interaction.guild.name);
-	},
+
+		const infoembed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle("Info")
+			.setImage('https://i.imgur.com/26UBWBp.gif')
+			.addFields(
+				{name: "Do you like programming?", value: "xmgg"}
+			)
+		
+		
+		await interaction.reply({ 
+			content: "something",
+			embeds: [infoembed],
+			ephemeral: true
+		});
+		
+	}
 };
