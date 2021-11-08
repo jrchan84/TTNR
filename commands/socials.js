f53c7e6d1a02d52d0c2e366ee6caa730590949c6
Trisha <trisha.d.sia@gmail.com>
2021-11-08 10:52:31 -0800

    add embed to bot message

--- commands/socials.js
+++ commands/socials.js
@@ -1,19 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fs = require('fs');
+const { MessageEmbed } = require('discord.js');
 
-let rawdata = fs.readFileSync('./public/socials.json');
-let social = JSON.parse(rawdata);
+let rawData = fs.readFileSync('./public/socials.json');
+let social = JSON.parse(rawData);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('socials')
 		.setDescription('Replies with DSC\'s socials!'),
 	async execute(interaction) {
-		await interaction.reply('Website: ' + social.website + '\n' +
-                            'Email: ' + social.email + '\n' +
-                            'Instagram: ' + social.instagram + '\n' +
-                            'Linkedin: ' + social.linkedin + '\n' +
-                            'Github: ' + social.github + '\n');
+
+		const socialEmbed = new MessageEmbed()
+			.setColor('#34A853')
+			.setTitle('Social Links')
+			.setDescription('a list of UBC DSC\'s socials')
+			.setThumbnail('https://i.ibb.co/Dz11mV3/DSC-logo-color.png')
+			.addFields(
+				{ name: 'Website', value: social.website },
+				{ name: 'Email', value: social.email },
+				{ name: 'Instagram', value: social.instagram },
+				{ name: 'Linkedin', value: social.linkedin },
+				{ name: 'Github', value: social.github }
+			)
+
+		await interaction.reply({ embeds: [socialEmbed] });
+
     console.log('TTNR replied with social links to: ' + interaction.user.tag
 			+ ' in server: ' + interaction.guild.name);
 	},
